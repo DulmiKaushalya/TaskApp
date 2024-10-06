cab97a6d9efedecdd5d74c78f5f61be2bb3255b6
DulmiKaushalya <146383047+DulmiKaushalya@users.noreply.github.com>
2024-10-06 23:26:57 +0530

    new

--- app/src/main/java/com/example/taskapp/AddNoteActivity.kt
+++ app/src/main/java/com/example/taskapp/AddNoteActivity.kt
@@ -11,7 +11,7 @@ class AddNoteActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_add_note)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.addNoteHeading)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
