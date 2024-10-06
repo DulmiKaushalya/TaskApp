be2e9cc7385bbea6f66dad0c702d938d21efcec7
DulmiKaushalya <146383047+DulmiKaushalya@users.noreply.github.com>
2024-10-07 02:49:33 +0530

    UpdateRead

--- app/src/main/java/com/example/taskapp/MainActivity.kt
+++ app/src/main/java/com/example/taskapp/MainActivity.kt
@@ -6,22 +6,35 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.taskapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private
-     lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var db: NoteDatabaseHelper
+    private lateinit var notesAdapter: NotesAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        db = NoteDatabaseHelper(this)
+        notesAdapter=NotesAdapter(db.getAllNotes(),this)
+
+        binding.notesRecyclerView.layoutManager=LinearLayoutManager(this)
+        binding.notesRecyclerView.adapter=notesAdapter
+
         binding.addButton.setOnClickListener{
             val intent = Intent(this,AddNoteActivity::class.java)
             startActivity(intent)
         }
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+        notesAdapter.refreshData(db.getAllNotes())
     }
 }
---
+++ app/src/main/java/com/example/taskapp/NotesAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.taskapp
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class NotesAdapter(private var notes:List<Note>,context: Context) : RecyclerView.Adapter<NotesAdapter.NoteViewHolder>() {
+
+     class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+         val titleTextView : TextView =itemView.findViewById(R.id.titleTextView)
+         val contentTextView:TextView = itemView.findViewById(R.id.contentTextView)
+     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+         val view = LayoutInflater.from(parent.context).inflate(R.layout.note_item,parent,false)
+        return  NoteViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = notes.size
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val note = notes[position]
+        holder.titleTextView.text=note.title
+        holder.contentTextView.text=note.content
+    }
+    fun refreshData(newNote: List<Note>){
+        notes=newNote
+        notifyDataSetChanged()
+    }
+}
