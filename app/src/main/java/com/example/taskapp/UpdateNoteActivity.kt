aa3345b7e7d01682a08ee93cad0b85dc7e0c5735
DulmiKaushalya <146383047+DulmiKaushalya@users.noreply.github.com>
2024-10-07 16:45:12 +0530

    update

--- app/src/main/java/com/example/taskapp/UpdateNoteActivity.kt
+++ app/src/main/java/com/example/taskapp/UpdateNoteActivity.kt
@@ -2,21 +2,18 @@ package com.example.taskapp
 
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.taskapp.databinding.ActivityUpdateBinding
+import com.example.taskapp.databinding.ActivityUpdateNoteBinding
 
 class UpdateNoteActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityUpdateBinding
+    private lateinit var binding: ActivityUpdateNoteBinding
     private lateinit var db : NoteDatabaseHelper
     private var noteId: Int = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
                super.onCreate(savedInstanceState)
-               binding = ActivityUpdateBinding.inflate(layoutInflater)
+               binding = ActivityUpdateNoteBinding.inflate(layoutInflater)
                setContentView(binding.root)
         db = NoteDatabaseHelper(this)
 
